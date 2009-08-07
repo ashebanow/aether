6b9be7d8b255f71e962c05ec50d12a2690c2d4eb
Andrew Shebanow <ashebanow@gmail.com>
2009-08-06 17:32:10 -0700

    shoulda is now a gem not a submodule plugin, and I unpacked all gems

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "authlogic-oid", :lib => "authlogic_openid"
+  config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
