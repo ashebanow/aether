4d8f27b0df6b5f706bb8e296f9ea15021c990104
Andrew Shebanow <ashebanow@gmail.com>
2009-08-08 16:34:29 -0700

    start adding feature steps

---
+++ features/step_definitions/character_management_steps.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
+And /^And I have not created any characters$/ do
+  pending
+end
+
+Then /^Then I should see an empty list of characters$/ do
+  pending
+end
+
+Then /^Then I should see my new character in the list$/ do
+  pending
+end
---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,5 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
+Given /^Given I am logged in$/ do
+  pending
+end
