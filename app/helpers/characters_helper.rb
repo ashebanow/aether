1a51257567f01bb49be485f0226e6f56a7089896
Andrew Shebanow <ashebanow@gmail.com>
2009-08-19 21:03:13 -0700

    start displaying more char info in show action

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -2,5 +2,18 @@ module CharactersHelper
   def format_modifier(abbrev)
     # TODO: implement me again!
   end
+  
+  def char_class_summary(character)
+    # turn a character's character class info into an abbreviated form, like
+    # "Warmain/7 Unfettered/3"
+    summary = ""
+    return "(No character classes!)" if character.char_classes.size == 0
+    for char_class in character.char_classes
+      summary += ' ' if s.length > 0
+      # FIXME: the Character model doesn't handle the join table right so we can't
+      # get the level
+      summary += char_class.name + '/' + '?'
+    end
+  end
 
 end
