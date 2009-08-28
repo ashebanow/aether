da914a329a432e323bca17c62de7dc1d3f58b508
Andrew Shebanow <ashebanow@gmail.com>
2009-08-28 10:55:21 -0700

    add timestamps to level as well

--- db/migrate/20090828162940_levels_as_list.rb
+++ db/migrate/20090828162940_levels_as_list.rb
@@ -2,11 +2,13 @@ class LevelsAsList < ActiveRecord::Migration
   def self.up
     remove_column :levels, :level
     add_column :levels, :position, :integer
+    add_timestamps :levels
     add_column :levels, :hit_points_added, :integer, :default => 0 
     add_index :levels, :position
   end
 
   def self.down
+    remove_timestamps :levels
     remove_index :levels, :position
     remove_column :hit_points_added, :position
     remove_column :levels, :position
